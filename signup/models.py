a7af1aa0e088ba107cac2da5257eff600c58778b
minjoo12 <hope_0726@hanmail.net>
2021-06-25 13:11:43 +0900

    likelion-7th-assignment

---
+++ signup/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Signup(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    pub_date = models.DateTimeField()
+    email = models.EmailField(max_length=100)
+    introduce = models.TextField()
+    image = models.ImageField(upload_to='images/', default='')
+
+    def info_summary(self):
+        return self.introduce[:100]
